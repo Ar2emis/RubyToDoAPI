60ea3dc571b51805705be4aa85dd4a6ee6ec1070
Artem Shevchenko <artemsheva0510@gmail.com>
2020-03-23 13:01:33 +0200

    refactored m2m tasks field display in project menu

--- api_server/todo_list/admin.py
+++ api_server/todo_list/admin.py
@@ -2,9 +2,14 @@ from django.contrib import admin
 from .models import Project, Task
 
 
+class ProjectTasksInline(admin.TabularInline):
+    model = Project.tasks.through
+    verbose_name = 'task'
+
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
-    pass
+    exclude = ['tasks']
+    inlines = [ProjectTasksInline]
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
